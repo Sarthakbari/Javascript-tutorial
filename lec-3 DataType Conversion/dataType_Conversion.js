10155dc052586d3db456c37f257d24f3547173c5
$arthakBari <barisarthak2005@gmail.com>
2025-10-04 06:59:53 +0000

    container config done

--- lec-3 DataType Conversion/dataType_Conversion.js
+++ lec-3 DataType Conversion/dataType_Conversion.js
@@ -29,7 +29,6 @@
 // "Sarthak" => true
 
 
-
 // Operations And String
 let someNumber = 13
 
@@ -37,37 +36,3 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
-// *************** Operations **************
-
-let value = 3 
-let negValue = -value
-// console.log(negValue);
-
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**2);
-// console.log(2/2);
-// console.log(2%2);
-
-let str1 = "Sarthak"
-let str2 = "Bari"
-
-// console.log(str1 + str2);
-let str3 = str1 + str2 
-console.log(str3);
-
-
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
-// console.log((3 + 5) * 7 % 2);
-
-// console.log(+true);
-// console.log(+"");
-
-let gameCounter = 100
-gameCounter++;
-// ++gameCounter;
-console.log(gameCounter);
---
+++ lec-4 Operations/operations.js
@@ -0,0 +1,34 @@
+// *************** Operations **************
+
+let value = 3 
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Sarthak"
+let str2 = "Bari"
+
+// console.log(str1 + str2);
+let str3 = str1 + str2 
+console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3 + 5) * 7 % 2);
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+// ++gameCounter;
+console.log(gameCounter);
