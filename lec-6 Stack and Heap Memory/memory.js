8314714f1fc5908fd35794de6f9f7b2f98c2739c
$arthakBari <barisarthak2005@gmail.com>
2025-10-05 06:16:03 +0000

    container config done

--- lec-6 Stack and Heap Memory/memory.js
+++ lec-6 Stack and Heap Memory/memory.js
@@ -1,28 +1,24 @@
 // memories: stack and heap
 
-// stack (primitive)
-// heap (non-primitive)
+// Stack (Primitive), Heap (Non-Primitive)
 
+// Ex
 
-// stack
-let myName = "Megh Bari"
+let myChannel = "Sarthakbaridotcom"
 
-let anotherName = myName
-anotherName = "meghhhhhh"
-
+let anotherName = myChannel
+anotherName = "Tere-----Channel"
+console.log(myChannel);
 console.log(anotherName);
-console.log(myName);
 
-
-
-// heap
 let userOne = {
-    email: "user@gmail.com",
-    upi : "user@sbi1234",
+    email: "user@google.com",
+    upi: "user@bkl"
 }
 
-let userTwo = userOne
-userTwo.email = "sarthak@google.com"
+let userTwo = userOne 
+userTwo.email = "Sarthak@google.com"
+console.log(userTwo);
 
 console.log(userOne.email);
 console.log(userTwo.email);
